bfc37ac7313d2ae5d10ec4f387b2ae4cbdada051
MariannaRaitamaa <163848017+MariannaRaitamaa@users.noreply.github.com>
2024-12-15 14:24:12 +0200

    Kommenttien lisäys

--- ohjelmointiprojekti.py
+++ ohjelmointiprojekti.py
@@ -1,33 +1,36 @@
-# Ladataan pääpäiväkirjan sisältö
+# Haetaan päiväkirjan sisältö
 with open("paivakirja.txt") as tiedosto:
     paivakirjateksti = [rivi.replace("\n", "") for rivi in tiedosto]
  
-# Ladataan ystäväsivun sisältö
+# Haetaan ystäväsivun sisältö
 with open("ystavasivu.txt") as tiedosto:
     ystavasivuteksti = [rivi.replace("\n", "") for rivi in tiedosto]
 
+# Haetaan osoitekirjan sisältö
 with open("osoitekirja.txt") as tiedosto:
     osoitekirjatiedot = [rivi.replace("\n", "") for rivi in tiedosto]
 
+# Alustetaan muuttujat
 paivakirjateksti = []
 ystavasivuteksti = [] 
 osoitekirjatiedot = []
 lista = []
 rivit = []
-# Avataan molemmat tiedostot lisäystä varten
+# Avataan tiedostot lisäystä varten
 while True:
-    print("1 - Päiväkirja, 2 - Ystäväsivu, 3 - Osoitekirja, 4 - Lopeta")
+    print("1 - Päiväkirja, 2 - Ystäväsivu, 3 - Osoitekirja, 4 - Lopeta") # Valikko
     toiminto = input("Valitse toiminto: ")
     if toiminto == "1":  # Päiväkirja
         with open("paivakirja.txt", "a+") as tiedosto:
             tiedosto.seek(0)
             for rivi in tiedosto:
-                paivakirjateksti.append(rivi.strip())  # Merkinnät listaan
+                paivakirjateksti.append(rivi.strip())  # Lisätään merkinnät listaan
             
             while True:
-                print("1 - Lisää teksti, 2 - Hae merkintä, 3 - Alkuun")
+                print("1 - Lisää teksti, 2 - Hae merkintä, 3 - Alkuun") # Alavalikko
                 toiminto2 = input("Valitse toiminto: ")
                 
+                # Lisätään päiväkirjamerkintä
                 if toiminto2 == "1":
                     paiva = input("Kirjoita päivämäärä (YYYY-MM-DD): ")
                     teksti = input("Kirjoita teksti: ")
@@ -36,6 +39,7 @@ while True:
                     paivakirjateksti.append(merkinta)
                     print("Teksti tallennettu")
                 
+                # Haetaan päiväkirjamerkintä päivämäärän perusteella
                 elif toiminto2 == "2":
                     paivamaara = input("Kirjoita päivämäärä (YYYY-MM-DD): ")
                     hakutulos = [merkinta for merkinta in paivakirjateksti if merkinta.startswith(paivamaara)]
@@ -44,13 +48,13 @@ while True:
                         print("Päiväkirjan teksti:")
                         for merkinta in hakutulos:
                             print(merkinta)
-
+                    # Jos päivämäärällä ei löytynyt merkintöjä
                     else:
                         print("Päivämäärällä ei löytynyt merkintöjä.")
                 
                 elif toiminto2 == "3":
                     break
-
+                # Virheellinen valinta, jos syöttää valikkoon jonkun muun kuin 1, 2 tai 3
                 else:
                     print("Virheellinen valinta, syötä 1, 2 tai 3.")
 
@@ -58,10 +62,11 @@ while True:
         with open("ystavasivu.txt", "a+") as tiedosto:
             tiedosto.seek(0)
             for rivi in tiedosto:
-                ystavasivuteksti.append(rivi.strip())  # Merkinnät listaan
+                ystavasivuteksti.append(rivi.strip())  # Lisätään merkinnät listaan
             while True:
-                print("1 - Lisää ystävä, 2 - Hae merkintä, 3 - Alkuun")
+                print("1 - Lisää ystävä, 2 - Hae merkintä, 3 - Alkuun") # Alavalikko
                 toiminto2 = input("Valitse toiminto: ")
+                # Lisätään ystävän tiedot
                 if toiminto2 == "1":
                     nimi = input("Nimi: ")
                     syntymavuosi = int(input("Syntymävuosi: "))
@@ -74,10 +79,10 @@ while True:
 
                     merkinta = (nimi + "\n" + str(syntymavuosi) + "\n" + harrastukset + "\n" + lempivari + "\n" + lempiruoka + "\n" + ammatti + "\n" + tapaaminen + "\n" + terveiset + "\n")
                     tiedosto.write(merkinta + "\n")
-                    ystavasivuteksti.append(merkinta.strip())
+                    ystavasivuteksti.append(merkinta.strip()) # Tallennetaan merkintä listaan
 
                     print("Ystävän tiedot tallennettu")
-
+                # Haetaan ystävän tiedot nimen perusteella
                 elif toiminto2 == "2":
                     nimi = input("Nimi: ")
                     hakutulos = [merkinta for merkinta in ystavasivuteksti if merkinta.startswith(nimi)]
@@ -85,13 +90,13 @@ while True:
                     if hakutulos:
                         print("Ystäväsivun tiedot:")
                         print("Nimi: " + nimi + "\n" + "Syntymävuosi: " + str(syntymavuosi) + "\n" + "Harrastukset: " + harrastukset + "\n" + "Lempiväri: " + lempivari + "\n" + "Lempiruoka: " + lempiruoka + "\n" + "Ammatti: " + ammatti + "\n" + "Missä olette tavanneet: " + tapaaminen + "\n" + "Terveisiä ystävälle: " + terveiset)
-
+                    # Jos nimen perusteella ei löytynyt ystävää, tulostuu
                     else:
                         print("Tällä nimellä ei löytynyt ystävää.")
 
                 elif toiminto2 == "3":
                     break
-
+                # Virheellinen valinta, jos syöttää valikkoon jonkun muun kuin 1, 2 tai 3
                 else:
                     print("Virheellinen valinta, syötä 1, 2, tai 3.")
 
@@ -100,12 +105,12 @@ while True:
             tiedosto.seek(0)
             for rivi in tiedosto:
                 if rivi.strip():
-                    osoitekirjatiedot.append(rivi.strip())  # Merkinnät listaan
+                    osoitekirjatiedot.append(rivi.strip())  # Lisätään merkinnät listaan
             
         while True:
             print("1 - Lisää osoitetiedot, 2 - Hae osoitetiedot, 3 - Alkuun")
             toiminto2 = input("Valitse toiminto: ")
-
+            # Lisätään osoitetiedot
             if toiminto2 == "1":
                 etunimi = input("Etunimi: ")
                 sukunimi = input("Sukunimi: ")
@@ -114,15 +119,15 @@ while True:
                 postinumero = int(input("Postinumero: "))
                 paikkakunta = input("Paikkakunta: ")
                 puh = int(input("Puhelinnumero: "))
-
+                # Muokataan osoitetiedot listaksi ja tallennetaan tiedostoon
                 merkinta = f"{etunimi}\n{sukunimi}\n{kadun_nimi}\n{talon_numero}\n{postinumero}\n{paikkakunta}\n{puh}\n"f"{etunimi}\n{sukunimi}\n{kadun_nimi}\n{talon_numero}\n{postinumero}\n{paikkakunta}\n{puh}\n"
                 
                 with open("osoitekirja.txt", "a+") as tiedosto:
                     tiedosto.write(merkinta + "\n")
-                    osoitekirjatiedot.append(merkinta.strip())
+                    osoitekirjatiedot.append(merkinta.strip()) # Tallennetaan merkinnät
 
                 print("Osoitetiedot tallennettu")
-
+            # Haetaan osoitetiedot etunimen perusteella
             elif toiminto2 == "2":
                 etunimi = input("Etunimi: ")
                 hakutulos = [merkinta for merkinta in osoitekirjatiedot if merkinta.startswith(etunimi)]
@@ -131,7 +136,7 @@ while True:
                     print("Osoitetiedot:")
                     for merkinta in hakutulos:
                         rivit = merkinta.split("\n")
-
+                        # Tulostetaan listana
                         if len(rivit) == 7:
                             print("Etunimi: " + rivit[0])
                             print("Sukunimi: " + rivit[1])
@@ -140,19 +145,19 @@ while True:
                             print("Postinumero: " + rivit[4])
                             print("Paikkakunta: " + rivit[5])
                             print("Puhelinnumero: " + rivit[6])
-
+                # Jos etunimellä ei löytynyt tietoja
                 else:
                     print("Tällä nimellä ei löytynyt tietoja.")
 
             elif toiminto2 == "3":
                 break
-
+            # Virheellinen valinta, jos syöttää valikkoon jonkun muun kuin 1, 2 tai 3
             else:
                 print("Virheellinen valinta, syötä 1, 2, tai 3.")
- 
+    # Lopetus
     elif toiminto == "4":
         print("Näkemiin, toivottavasti nähdään huomenna!")
         break
-
+    # Virheellinen valinta, jos syöttää valikkoon jonkun muun kuin 1, 2, 3 tai 4
     else:
         print("Virheellinen valinta, syötä 1, 2, 3 tai 4.")
